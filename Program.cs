2a84449aaad59cc10ec7d51a58839e9fb67b8ccd
Silvio Stefane Ferreira barbosa <ssfbarbosa@outlook.com>
2023-05-30 12:55:18 -0300

    Primeira versão de testes para rodar no raspberry

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var temp = GetCpuTemperature();
+
+        if (temp.HasValue)
+        {
+            Console.WriteLine($"CPU Temperature: {temp.Value}°C");
+        }
+        else
+        {
+            Console.WriteLine("Failed to get CPU temperature.");
+        }
+    }
+
+    static float? GetCpuTemperature()
+    {
+        try
+        {
+            using var reader = new StreamReader("/sys/class/thermal/thermal_zone0/temp");
+            var line = reader.ReadLine();
+            if (line != null && float.TryParse(line, out var temp))
+            {
+                return temp / 1000;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading CPU temperature. {ex.Message}");
+        }
+
+        return null;
+    }
+}
